34227622adb505d160fa7918a15db82ad67c439b
bautiarmanicode <bautiarmanienergia@gmail.com>
2024-05-24 17:16:12 -0300

    .

--- main.py
+++ main.py
@@ -54,32 +54,7 @@ def userdata(user_id: str):
 
 
 '''
-
-
-
-@app.get("/userdata/{user_id}",response_model=List,  
-            description="""
-    <font color="blue">
-        INSTRUCCIONES<br>
-        1. Haga clic en "Try it out".<br>
-        2. Ingrese el user_id en el cuadro de abajo.<br>
-        3. Desplácese hacia "Resposes" para ver `cantidad` de dinero gastado por el usuario, el `porcentaje` de recomendación en base a reviews.recommend y `cantidad de juegos.<br>
-        4_ Ejemplos de desarrolladores para consultar: evcentric, 76561198099295859
-    </font>
-""", 
-tags=["Consultas Generales"])
-
-def userdata(user_id: str):    
-    user = fa.userdata(user_id)
-    return user
-
-
-
-
-# _________________________________________________________________________________________________________________________________
-
-
-
+ME QUEDO PENDIENTE
 
 # Función para el endpoint UserForGenre
 @app.get("/UserForGenre/{genero}")
