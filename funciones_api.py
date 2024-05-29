05ac61da6772706efdb397d2d6988e8d0839eead
bautiarmanicode <bautiarmanienergia@gmail.com>
2024-05-29 00:47:13 -0300

    ✅

--- funciones_api.py
+++ funciones_api.py
@@ -73,14 +73,23 @@ def intro():
     '''
 
 def developer(desarrollador: str):
+    """
+    Función que filtra el DataFrame df_developer por el desarrollador proporcionado y devuelve una lista de diccionarios con las columnas 'Año', 'Cantidad de Items' y 'Contenido Free'.
+
+    Args:
+        desarrollador (str): Nombre del desarrollador a filtrar.
+
+    Returns:
+        list: Lista de diccionarios con las columnas 'Año', 'Cantidad de Items' y 'Contenido Free' para el desarrollador especificado.
+    """
     # Filtramos el DataFrame por el desarrollador
-    result = df_developer[df_developer['Desarrollador'] == desarrollador]
+    result = df_developer[df_developer["Desarrollador"] == desarrollador]
 
     # Seleccionamos solo las columnas necesarias
-    result = result[['Año', 'Cantidad de Items', 'Contenido Free']]
+    result = result[["Año", "Cantidad de Items", "Contenido Free"]]
     # Convertimos el resultado a una lista de diccionarios
-    resultado_final = result.to_dict(orient='records')
-     
+    resultado_final = result.to_dict(orient="records")
+
     # Liberamos la memoria utilizada por el DataFrame intermedio
     del result
     gc.collect()
@@ -92,90 +101,122 @@ def userdata(user_id: str):
     """
     Devuelve la cantidad de dinero gastado por el usuario ingresado, el porcentaje de recomendación sobre las reviews realizadas y la cantidad de items.
     Ejemplo de retorno: {"Usuario X": us213ndjss09sdf, "Dinero gastado": 200 USD, "% de recomendación": 20%, "cantidad de items": 5}
+
+    Args:
+        user_id (str): ID del usuario del cual se desea obtener la información.
+
+    Returns:
+        dict: Diccionario con la información del usuario, incluyendo el dinero gastado, el porcentaje de recomendación y la cantidad de items.
     """
     # Si el user_id no se encuentra en los dataframes:
-    if user_id not in df_user_reviews['user_id'].values:
+    if user_id not in df_user_reviews["user_id"].values:
         return f"ERROR: El user_id {user_id} no existe en la base de datos."  # se imprime mensaje de error
     else:
         # Se filtran los datos en función al usuario especificado
-        df_filtrado = df_user_reviews[df_user_reviews['user_id'] == user_id]
-        
+        df_filtrado = df_user_reviews[df_user_reviews["user_id"] == user_id]
+
         # Se unen las columnas necesarias de los dataframes:
-        df_merged = pd.merge(df_filtrado[['user_id', 'reviews_item_id', 'reviews_recommend']], 
-                              df_steam_games[['id', 'price']], 
-                              left_on='reviews_item_id', right_on='id', how='inner')
-        
+        df_merged = pd.merge(
+            df_filtrado[["user_id", "reviews_item_id", "reviews_recommend"]],
+            df_steam_games[["id", "price"]],
+            left_on="reviews_item_id",
+            right_on="id",
+            how="inner",
+        )
+
         # Se calcula la cantidad de dinero gastado por el usuario
-        dinero_gastado = round(df_merged['price'].sum(), 2)
-        
+        dinero_gastado = round(df_merged["price"].sum(), 2)
+
         # Se calcula la cantidad de recomendaciones del usuario
-        recomendaciones = df_merged['reviews_recommend'].sum()
-        
+        recomendaciones = df_merged["reviews_recommend"].sum()
+
         # Se calcula el total de reviews del usuario
         total_reviews = df_merged.shape[0]
-        
+
         # Se calcula el porcentaje de recomendaciones sobre el total de reviews
         porcentaje_recomendacion = round(recomendaciones / total_reviews * 100, 0)
-        
+
         # Se calcula la cantidad de items por usuario
-        cantidad_de_items = df_merged['reviews_item_id'].nunique()
-        
+        cantidad_de_items = df_merged["reviews_item_id"].nunique()
+
         # Crear un diccionario con los resultados
         dicc_rdos = {
             "Usuario": user_id,
-            "Dinero gastado": f'{dinero_gastado} USD',
-            "% de recomendación": f'{porcentaje_recomendacion}%',
-            'Cantidad de items': cantidad_de_items
+            "Dinero gastado": f"{dinero_gastado} USD",
+            "% de recomendación": f"{porcentaje_recomendacion}%",
+            "Cantidad de items": cantidad_de_items,
         }
         # Liberamos la memoria utilizada por el DataFrame intermedio
         del df_filtrado, df_merged
         gc.collect()
-        
+
         return dicc_rdos
 
 
 # ____________________________________________________________________________________
 def recomendacion_juego(user_id: str):
-    '''
+    """
     Devuelve una lista con 5 sugerencias de juegos para el usuario seleccionado.
     Ejemplo de retorno: {'Sugerencias para el usuario 76561197970982479': ['1. RWBY: Grimm Eclipse', '2. Rogue Legacy', '3. Dust: An Elysian Tail', "4. King Arthur's Gold", '5. RIFT']}
-    '''
+
+    Args:
+        user_id (str): El ID del usuario para el cual se desean obtener las recomendaciones de juegos.
+
+    Returns:
+        dict: Un diccionario con una clave que indica el usuario y un valor que es una lista con las 5 recomendaciones de juegos.
+    """
     # Si el ID de usuario no se encuentra en los dataframes:
-    if user_id not in df_user_reviews['user_id'].values:
+    if user_id not in df_user_reviews["user_id"].values:
         return f"ERROR: El ID de usuario {user_id} no existe en la base de datos."  # se imprime un mensaje de error
     else:
         # Se asigna el ID ingresado a la variable user
         user = user_id
 
         # En primer lugar, se extraen los juegos que el usuario ya ha jugado:
-        df_rev_games = pd.merge(df_user_reviews, df_steam_games, left_on="reviews_item_id", right_on="id", how="inner")
-        juegos_jugados = df_rev_games[df_rev_games['user_id'] == user]
+        df_rev_games = pd.merge(
+            df_user_reviews,
+            df_steam_games,
+            left_on="reviews_item_id",
+            right_on="id",
+            how="inner",
+        )
+        juegos_jugados = df_rev_games[df_rev_games["user_id"] == user]
 
         # Se eliminan del dataframe de juegos los jugados por el usuario
-        df_user = df_steam_games[["id", "app_name"]].drop(juegos_jugados.id, errors='ignore')
+        df_user = df_steam_games[["id", "app_name"]].drop(
+            juegos_jugados.id, errors="ignore"
+        )
 
         # Ruta completa al archivo RS_model.pkl
-        ruta_modelo = './0 Dataset/RS_model.pkl'
+        ruta_modelo = "./0 Dataset/RS_model.pkl"
 
         # Se carga el modelo de Sistema de Recomendación entrenado desde el archivo especificado
-        with open(ruta_modelo, 'rb') as file:
+        with open(ruta_modelo, "rb") as file:
             RS_model = pickle.load(file)
 
         # Se realizan las predicciones y se agregan en una nueva columna:
-        df_user['estimate_Score'] = df_user['id'].apply(lambda x: RS_model.predict(user, x).est)
+        df_user["estimate_Score"] = df_user["id"].apply(
+            lambda x: RS_model.predict(user, x).est
+        )
 
         # Se ordena el dataframe de manera descendente en función al score y se seleccionan los 5 principales:
-        sugerencias = df_user.sort_values('estimate_Score', ascending=False)["app_name"].head(5).to_list()
+        sugerencias = (
+            df_user.sort_values("estimate_Score", ascending=False)["app_name"]
+            .head(5)
+            .to_list()
+        )
 
         # Se crea la llave del diccionario de retorno
-        llave_dic = f'Sugerencias para el usuario {user}'
+        llave_dic = f"Sugerencias para el usuario {user}"
 
         # Se da formato a las 5 sugerencias:
-        sugerencias_formateadas = [f'{i+1}. {sugerencia}' for i, sugerencia in enumerate(sugerencias)]
+        sugerencias_formateadas = [
+            f"{i+1}. {sugerencia}" for i, sugerencia in enumerate(sugerencias)
+        ]
         # Se libera la memoria utilizada por los dataframes intermedios
         del df_rev_games, juegos_jugados, df_user
         gc.collect()
-        
+
         # Se devuelven los resultados en un diccionario
         return {llave_dic: sugerencias_formateadas}
 
@@ -183,11 +224,6 @@ def recomendacion_juego(user_id: str):
 
 
 
-# def recomendacion_juego(id_producto):
-    # Lógica para recomendar juegos similares al juego con id_producto
-    # Devolver una lista con 5 juegos recomendados
-    
-    
 # def recomendacion_usuario(id_usuario):
     # Lógica para recomendar juegos a un usuario específico
     # Devolver una lista con 5 juegos recomendados para el usuario id_usuario
